45b14acb125a01522ca5cb426a94665086e4df77
DominicKepes <143762188+DominicKepes@users.noreply.github.com>
2024-03-14 22:08:09 -0700

    Delete sensitive info

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
-require('dotenv').config()
 
-const uri = `mongodb+srv://dominickepes:${process.env.MONGO_PASS}@cluster0.egtkwdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
